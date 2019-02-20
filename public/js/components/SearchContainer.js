7f2a9e41c42d59e76b460f31c47980c07f1bf91e
YingCGooi <25574844+YingCGooi@users.noreply.github.com>
2019-02-20 17:22:26 -0500

    extract search form into an input component

--- public/js/components/SearchContainer.js
+++ public/js/components/SearchContainer.js
@@ -2,20 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import MovieItem from './MovieItem';
+import SearchInputForm from './SearchInputForm';
 
 const SearchContainer = ({ result, favoriteInfo, handleFavoriteButtonClicked }) => (
   <main id='search-container'>
-    <form>
-      <input
-        id='title'
-        name='title'
-        type='text'
-        placeholder='Search movie title'
-      />
-      <button type='submit'>
-        <i className='search icon'></i>
-      </button>
-    </form>
+    <SearchInputForm 
+      placeholder='Search movie title...'
+      onSubmit={() => ()}
+    />
     <MovieItem
       movie={ favoriteInfo ? favoriteInfo : result }
       isFavorite={ !!favoriteInfo }
