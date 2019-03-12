dc91ce5b1fddbd6777f565f7e43ce997a37ae160
YingCGooi <25574844+YingCGooi@users.noreply.github.com>
2019-03-11 23:34:34 -0400

    use if statement for action handler

--- public/js/reducers/searchReducer.js
+++ public/js/reducers/searchReducer.js
@@ -1,11 +1,3 @@
-// const initialState = {
-//   "title": "Star Wars: Episode IV - A New Hope",
-//   "year": "1977",
-//   "plot": "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.",
-//   "poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-//   "imdbID": "tt0076759"
-// }
-
 const initialState = {
   "title": "",
   "year": "",
@@ -15,12 +7,8 @@ const initialState = {
 }
 
 export default (state = initialState, action) => {
-  switch (action.type) {
-    case 'SEARCH_SUCCESS':
-      state = action.result;
-      break;
-      
-    default: return state;
+  if (action.type === 'SEARCH_SUCCESS') {
+    state = action.result;
   }
   return state;
 }
